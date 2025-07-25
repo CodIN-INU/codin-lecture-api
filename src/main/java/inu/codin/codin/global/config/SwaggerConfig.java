fe2233322b7087312ddbb57e7ac091a14aaba837
Kwak Byoung Min <rovin1273@gmail.com>
2025-07-25 16:01:51 +0900

    build: Swagger 버전 에러 수정 및 로그 레벨 조정

--- src/main/java/inu/codin/codin/global/config/SwaggerConfig.java
+++ src/main/java/inu/codin/codin/global/config/SwaggerConfig.java
@@ -26,16 +26,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         Info info = new Info()
-                .title("CODIN Ticketing Module API Documentation")
-                .description("CODIN Ticketing Module API 명세서")
+                .title("CODIN Lecture API Documentation")
+                .description("CODIN Lecture API 명세서")
                 .version("v1.0.0");
-
-        // Cookie Auth 설정
-        SecurityScheme cookieAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .in(SecurityScheme.In.COOKIE)
-                .name("access_token")
-                .description("쿠키를 통한 인증 (access_token)");
 
         // Bearer Token Auth 설정 (백업용)
         SecurityScheme bearerAuth = new SecurityScheme()
@@ -45,14 +38,12 @@ public class SwaggerConfig {
                 .description("Authorization 헤더를 통한 Bearer Token 인증");
 
         // Security Requirements 설정
-        SecurityRequirement cookieRequirement = new SecurityRequirement().addList("cookieAuth");
         SecurityRequirement bearerRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
                 .info(info)
-                .security(List.of(cookieRequirement, bearerRequirement))
+                .security(List.of(bearerRequirement))
                 .components(new Components()
-                        .addSecuritySchemes("cookieAuth", cookieAuth)
                         .addSecuritySchemes("bearerAuth", bearerAuth)
                 )
                 .servers(List.of(
