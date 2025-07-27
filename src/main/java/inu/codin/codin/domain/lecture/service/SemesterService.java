a6602b8949a77dc78f650a8a8ef723249e0446e3
X1n9fU <minkyeong0244@gmail.com>
2025-07-28 01:09:58 +0900

    refactor : semester 관련 책임 service로 이전

--- src/main/java/inu/codin/codin/domain/lecture/service/SemesterService.java
+++ src/main/java/inu/codin/codin/domain/lecture/service/SemesterService.java
@@ -17,10 +17,15 @@ public class SemesterService {
 
     public Optional<Semester> getSemester(String semester) {
         try {
-            int [] parsed = Semester.parseSemester(semester);
+            int [] parsed = parseSemester(semester);
             return semesterRepository.findSemesterByYearAndQuarter(parsed[0], parsed[1]);
         } catch (Exception e) {
             throw new SemesterException(SemesterErrorCode.SEMESTER_INVALID_FORMAT);
         }
+    }
+
+    public int[] parseSemester(String semesterStr) {
+        String[] parts = semesterStr.split("-");
+        return new int[]{Integer.parseInt(parts[0]), Integer.parseInt(parts[1])};
     }
 }
