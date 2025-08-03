478900c1bacc53745ea40efe7367e666af695042
Kwak Byoung Min <rovin1273@gmail.com>
2025-08-03 18:53:43 +0900

    comment: 추후 추가할 강의계획서 전체, AI 요약본 멤버 주석화

--- src/main/java/inu/codin/codin/domain/elasticsearch/document/LectureDocument.java
+++ src/main/java/inu/codin/codin/domain/elasticsearch/document/LectureDocument.java
@@ -57,4 +57,24 @@ public class LectureDocument {
 
     @Field(type = FieldType.Keyword)
     private List<String> semesters;
+
+    // 강의계획서 전체
+//    @Field(type = FieldType.Text,
+//            analyzer = "nori",
+//            searchAnalyzer = "nori",
+//            indexOptions = IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS,
+//            termVector = TermVector.WITH_POSITIONS_OFFSETS,
+//            fields = {
+//                    @InnerField(suffix = "keyword", type = FieldType.Keyword)
+//            })
+//    private String syllabus;  // 강의계획서 전체
+
+    // AI 요약본
+//    @Field(type = FieldType.Text,
+//            analyzer = "standard",
+//            searchAnalyzer = "standard",
+//            indexOptions = IndexOptions.POSITIONS,
+//            termVector = TermVector.NO,
+//            store = true)          // 자주 조회되는 요약본만 store
+//    private String aiSummary;
 }
