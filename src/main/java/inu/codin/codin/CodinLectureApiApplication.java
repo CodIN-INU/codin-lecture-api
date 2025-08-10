100e8b0f59b365290b2d523465d9a1f48e4c1e45
Kwak Byoung Min <rovin1273@gmail.com>
2025-08-10 21:49:06 +0900

    refactor: TimeZeon Asia/Seoul로 설정

--- src/main/java/inu/codin/codin/CodinLectureApiApplication.java
+++ src/main/java/inu/codin/codin/CodinLectureApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,6 +13,7 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 public class CodinLectureApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(CodinLectureApiApplication.class, args);
     }
 
