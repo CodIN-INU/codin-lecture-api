53a548359f7c0b06bb9c1882448251c37ff29673
Kwak Byoung Min <rovin1273@gmail.com>
2025-08-20 00:37:56 +0900

    refactor: 콤마를 통해 String List Converter 관리하도록 설정

--- src/main/java/inu/codin/codin/domain/lecture/converter/StringListConverter.java
+++ src/main/java/inu/codin/codin/domain/lecture/converter/StringListConverter.java
@@ -1,32 +1,35 @@
 package inu.codin.codin.domain.lecture.converter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import inu.codin.codin.domain.lecture.exception.LectureErrorCode;
+import inu.codin.codin.domain.lecture.exception.LectureException;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Converter(autoApply = true)
 public class StringListConverter implements AttributeConverter<List<String>, String> {
-
-    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public String convertToDatabaseColumn(List<String> dataList) {
         try {
-            return mapper.writeValueAsString(dataList);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            return String.join(",", dataList);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new LectureException(LectureErrorCode.CONVERT_ERROR);
         }
     }
 
     @Override
     public List<String> convertToEntityAttribute(String data) {
         try {
-            return mapper.readValue(data, List.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+            return data != null ? Arrays.stream(data.split(",")).toList() : null;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new LectureException(LectureErrorCode.CONVERT_ERROR);
         }
     }
 }
