e158d27bfa5a0aa8a32f15283d4507fab139550c
Kwak Byoung Min <rovin1273@gmail.com>
2025-09-08 15:45:30 +0900

    fix: Evaluation 컨버터 추가 및 Type Enumerated 삭제
    - Evaluation 컨버터가 삭제되어서 JPA no enum constant  에러 발생
    - Type 또한 Enumerated 어노테이션이 있어서 no enum constant 에러 발생
    
    전에 수정한 코드 부분을 다시 바꿨습니다.

--- src/main/java/inu/codin/codin/domain/lecture/converter/EvaluationConverter.java
+++ src/main/java/inu/codin/codin/domain/lecture/converter/EvaluationConverter.java
@@ -1,18 +1,31 @@
 package inu.codin.codin.domain.lecture.converter;
 
 import inu.codin.codin.domain.lecture.entity.Evaluation;
+import inu.codin.codin.domain.lecture.exception.LectureErrorCode;
+import inu.codin.codin.domain.lecture.exception.LectureException;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Converter(autoApply = true)
 public class EvaluationConverter implements AttributeConverter<Evaluation, String> {
     @Override
     public String convertToDatabaseColumn(Evaluation attribute) {
-        return attribute.getDescription();
+        return attribute != null ? attribute.getDescription() : null;
     }
 
     @Override
-    public Evaluation convertToEntityAttribute(String dbData) {
-        return Evaluation.fromDescription(dbData);
+    public Evaluation convertToEntityAttribute(String data) {
+        if (data == null || data.trim().isEmpty()) {
+            return null;
+        }
+
+        try {
+            return Evaluation.fromDescription(data.trim());
+        } catch (IllegalArgumentException e) {
+            log.warn("Evaluation Converter 부분에서 문제가 발생했습니다.");
+            throw new LectureException(LectureErrorCode.CONVERT_ERROR);
+        }
     }
 }
