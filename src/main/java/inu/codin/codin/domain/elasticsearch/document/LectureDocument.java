e3bc683b654b2b6a3c38ef228e2312f321c57c54
BHC-Chicken <tij05078@gmail.com>
2025-11-04 01:54:17 +0900

    refactor : LectureDocument 수정
    - Java client 로 변경하며 더이상 사용되지 않는 필드를 제거하고 jackson 매핑을 위한 빈 생성자를 추가했습니다.

--- src/main/java/inu/codin/codin/domain/elasticsearch/document/LectureDocument.java
+++ src/main/java/inu/codin/codin/domain/elasticsearch/document/LectureDocument.java
@@ -3,93 +3,32 @@ package inu.codin.codin.domain.elasticsearch.document;
 import inu.codin.codin.domain.elasticsearch.document.dto.EmotionInfo;
 import inu.codin.codin.domain.elasticsearch.document.dto.ScheduleInfo;
 import inu.codin.codin.domain.elasticsearch.document.dto.SemesterInfo;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.*;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Builder
-@Document(indexName = "lectures")
-@Setting(settingPath = "elasticsearch/settings/lecture-settings.json")
+@NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class LectureDocument {
-
-    @Id
     private Long id;
-
-    @Field(type = FieldType.Text, analyzer = "nori_autocomplete", searchAnalyzer = "nori")
-    private String lectureNm;
-
-    @Field(type = FieldType.Integer)
+    private String lectureName;
     private Integer grade;
-
-    @Field(type = FieldType.Integer)
     private Integer credit;
-
-    @Field(type = FieldType.Keyword)
     private String professor;
-
-    @Field(type = FieldType.Keyword)
     private String department;
-
-    @Field(type = FieldType.Keyword)
     private String type;
-
-    @Field(type = FieldType.Keyword)
     private String lectureType;
-
-    @Field(type = FieldType.Keyword)
     private String evaluation;
-
-    @Field(type = FieldType.Text, analyzer = "nori")
     private List<String> preCourses;
-
-    @Field(type = FieldType.Double)
     private Double starRating;
-
-    @Field(type = FieldType.Integer)
     private Integer likes;
-
-    @Field(type = FieldType.Integer)
     private Integer hits;
-
-    @Field(type = FieldType.Nested)
     private List<SemesterInfo> semesters;
-
-    @Field(type = FieldType.Keyword)
     private List<String> tags;
-
-    @Field(type = FieldType.Nested)
     private List<ScheduleInfo> schedule;
-
-    @Field(type = FieldType.Object)
     private EmotionInfo emotion;
-
-    // 강의계획서 전체
-    @MultiField(
-            mainField = @Field(type = FieldType.Text,
-                    analyzer = "nori",
-                    searchAnalyzer = "nori",
-                    indexOptions = IndexOptions.offsets,
-                    termVector = TermVector.with_positions_offsets),
-            otherFields = {
-                    @InnerField(suffix = "keyword", type = FieldType.Keyword)
-            }
-    )
     private String syllabus;
-
-    //     AI 요약본
-    @Field(type = FieldType.Text,
-            analyzer = "standard",
-            searchAnalyzer = "standard",
-            indexOptions = IndexOptions.positions,
-            termVector = TermVector.no,
-            store = true)
     private String aiSummary;
 }
