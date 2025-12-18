da9e95422fbd74a0dc5dc42d512744b17af2e9cf
BHC-Chicken <tij05078@gmail.com>
2025-12-19 01:25:59 +0900

    feat: 외부 요청 추적을 위한 트랜잭션 ID 전파 로직 구현
    - 분산 추적을 위해 트랜잭션 ID 처리 로직 개선
    - LogConstant 상수 클래스 추가 (MDC 및 Header Key 관리)
    - MdcLoggingInterceptor 수정:
      1. 요청 헤더(X-Request-ID)에 txId가 존재하면 그대로 사용 (로그 연결)
      2. 존재하지 않으면 새로 생성 (UUID)
      3. 응답 헤더에도 txId를 포함하여 클라이언트가 추적 가능하도록 변경

--- src/main/java/inu/codin/codin/global/common/interceptor/MdcLoggingInterceptor.java
+++ src/main/java/inu/codin/codin/global/common/interceptor/MdcLoggingInterceptor.java
@@ -1,6 +1,7 @@
 package inu.codin.codin.global.common.interceptor;
 
 
+import inu.codin.codin.global.common.constant.LogConstant;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.Nullable;
@@ -14,8 +15,18 @@ import java.util.UUID;
 public class MdcLoggingInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String txId = UUID.randomUUID().toString().substring(0, 8);
-        MDC.put("txId", txId);
+        String requestTxId = request.getHeader(LogConstant.HEADER_TX_ID);
+        String txId;
+
+        if (requestTxId != null && !requestTxId.isEmpty()) {
+            txId = requestTxId;
+        } else {
+            txId = UUID.randomUUID().toString().substring(0, 8);
+        }
+
+        MDC.put(LogConstant.MDC_TX_ID, txId);
+
+        response.setHeader(LogConstant.HEADER_TX_ID, txId);
 
         return true;
     }
